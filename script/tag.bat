c56b1fdc0baff385b86a233517dcff40ea544fe0
wang.qiyuan <wang.qiyuan@21vianet.com>
2026-04-13 10:48:32 +0800

    chore: update go.mod and go.sum for dependency version consistency and tagging scripts

--- script/tag.bat
+++
@@ -1,4 +0,0 @@
-git tag tracing/v1.1.1 --force
-git tag testing/v1.1.1 --force
-
-git push origin --tags
---
+++ script/tag.sh
@@ -0,0 +1,4 @@
+git tag -f tracing/v1.0.0
+git tag -f testing/v1.0.0
+
+git push origin --tags -f
